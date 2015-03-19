8caa434e1a361df745ff88baced43bb37ed39ce1
wagppinto <wagpinto@me.com>
2015-03-19 09:29:57 -0600

    Initial Commit

---
+++ GitReview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitReview
+//
+//  Created by Wagner Pinto on 3/19/15.
+//  Copyright (c) 2015 Wagner Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitReview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitReview
+//
+//  Created by Wagner Pinto on 3/19/15.
+//  Copyright (c) 2015 Wagner Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
